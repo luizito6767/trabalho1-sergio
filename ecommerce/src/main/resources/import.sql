951cfb8d881791f13ff6478a9053df2d0b3d5cef
Sérgio Roberto Delfino <sergio.delfino@fatecourinhos.edu.br>
2026-08-27 01:37:51 +0000

    Atualizações

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into categoria (nome, descricao) values ('Informática', 'Produtos de Informática');
+insert into categoria (nome, descricao) values ('Livros', 'Livros Técnicos');
+
+insert into produto (nome, descricao, preco, estoque, categoria_id) values ('Código Limpo', 'Livro do Autor Robert C. Martin', 87.34, 20, 2);
